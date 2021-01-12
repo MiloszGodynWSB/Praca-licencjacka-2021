42207055370f5a86079fd5f30317b3f08ec4d5e3
Miłosz Godyń <milgodyn@outlook.com>
2021-01-12 18:00:12 +0100

    Add repository annotation to UserRepository

--- src/main/java/pl/wsb/licencjat/repository/UserRepository.java
+++ src/main/java/pl/wsb/licencjat/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package pl.wsb.licencjat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.wsb.licencjat.model.database.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
